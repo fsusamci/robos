38eb15dc182f8e528087a3d787ca5bb93629c7d8
Furkan Susamci <fsusamci.coding@gmail.com>
2018-04-26 23:26:41 +0300

    Added username attribute to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Card = ({ name, email, id }) => {
+const Card = ({ name, email, id, username }) => {
   return (
     <div className='tc grow bg-washed-blue br3 pa3 ma2 dib bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
+        <p>{username}</p>
         <p>{email}</p>
       </div>
     </div>
